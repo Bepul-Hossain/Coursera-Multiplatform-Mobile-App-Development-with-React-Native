c0849fdde342f2c2d7be8404087a3396b6b2be1d
bepul0 <bepul.cse.ru@gmail.com>
2020-07-02 15:35:15 +0600

    Initial set up complete

--- App.js
+++ App.js
@@ -1,16 +1,23 @@
-import * as React from "react";
-import { View, Text } from "react-native";
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
-export default function App() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Universal React with Expo</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>OPEN</Text>
+        <Text>OPEN</Text> 
+        <Text>OPEN</Text>
+        <Text>OPEN</Text>
+      </View>
+    );
+  }
 }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems:'center',
+    justifyContent: 'center'
+  },
+});
