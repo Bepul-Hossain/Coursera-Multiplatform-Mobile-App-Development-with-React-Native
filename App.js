f6944847a6f2058361bf88093d7c2f96a7a0fbc2
bepul0 <bepul.cse.ru@gmail.com>
2020-07-05 10:35:38 +0600

    ok

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  View,
+  Button,
+  TextInput,
+  StyleSheet,
+  Text,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import GoalItem from "./components/GoalItem";
+import GoalInput from "./components/GoalInput";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>OPEN</Text>
-        <Text>OPEN</Text> 
-        <Text>OPEN</Text>
-        <Text>OPEN</Text>
-      </View>
-    );
-  }
+export default function App() {
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const addGoal = (goalTitle) => {
+    setCourseGoals((currnetGoals) => [
+      ...currnetGoals,
+      { id: Math.random().toString(), value: goalTitle },
+    ]);
+  };
+  return (
+    <View style={styles.screen}>
+      <GoalInput onAddGoal={addGoal} />
+      <FlatList
+        keyExtractor={(item, index) => item.id}
+        data={courseGoals}
+        renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
+      />
+    </View>
+  );
 }
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems:'center',
-    justifyContent: 'center'
+  screen: {
+    padding: 25,
   },
 });
